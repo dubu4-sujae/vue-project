b339c62598d14c41132c20dbab2546dd2be98d64
sujea777 <39877700+sujea777@users.noreply.github.com>
2019-12-09 12:29:21 +0900

    1. test

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="wrap">
+    <nav class="nav border border-danger">
+      <div class="container border border-primary">
+        <div class="nav_1 border border-danger">
+          
+        </div>
+        <div class="nav_2 border border-danger"></div>
+        <div class="nav_3 border border-danger"></div>
+      </div>
+    </nav>
+
+    <div class="product_ad border border-danger">
+
+    </div>
+
+    <div class="product border border-dangers">
+      <div class="container border border-danger">
+        <h1 class="text-center h1_pt2">Product 1 section</h1>
+        <div class="product_div border "></div>
+        <div class="product_div border"></div>
+        <div class="product_div border"></div>
+        <div class="product_div border"></div>
+      </div>
+    </div>
+
+    <div class="time_sale border border-danger">
+      <div class="container border border-danger">
+        <div class="time_sale_div1 border">
+
+        </div>
+        <div class="time_sale_div2 border">
+
+        </div>
+      </div>
+    </div>
+
+
+    <div class="event border">
+      <div class="container border">
+        <h1 class="text-center h1_pt2">이벤트 소식</h1>
+        <div class="event_div border"></div>
+        <div class="event_div border"></div>
+        <div class="event_div border"></div>
+      </div>
+    </div>
+
+    <div class="product border border-dangers">
+      <div class="container border border-danger">
+        <h1 class="text-center h1_pt2">Product 2 section</h1>
+        <div class="product_div border"></div>
+        <div class="product_div border"></div>
+        <div class="product_div border"></div>
+        <div class="product_div border"></div>
+      </div>
+    </div>
+
+    <div class="banner border">
+      <div class="container border"></div>
+    </div>
+
+    <div class="recommend border">
+      <div class="container border">
+        <h1 class="text-center h1_pt2"> MD의 추천</h1>
+        <ul class="list-unstyled recommned_ul">
+          <li>채소</li>
+          <li>과일·견과·쌀</li>
+          <li>수산·해산·건어물</li>
+          <li>정육·계란</li>
+          <li>국·반찬·메인요리</li>
+          <li>샐러드·간편식</li>
+          <li>면·양념·오일</li>
+          <li>음료·우유·떡·간식</li>
+          <li>베이커리·치즈·델리</li>
+          <li>건강식품</li>
+          <li>생활용품</li>
+          <li>주방용품</li>
+          <li>가전제품</li>
+          <li>베이비·키즈</li>
+          <li>반려동물</li>
+        </ul>
+      </div>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style src="./index.css">
+</style>
